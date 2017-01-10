992963eb4e4934971481f29a61fa6d1d91f54b1f
sensenario <sensenario@gmail.com>
2017-01-10 17:10:56 +0300

    updated base personal form for admin

--- src/vegetable.admin/wwwroot/app/dist/controllers/basePersonalInfoController.js
+++ src/vegetable.admin/wwwroot/app/dist/controllers/basePersonalInfoController.js
@@ -12,6 +12,28 @@ var AdminApp;
                 _this.availableTabs = data;
             });
         }
+        BasePersonalInfoController.prototype.queryTags = function (searchTag) {
+            var results;
+            var allTags = this.availableTabs.data;
+            if (!searchTag) {
+                // only return the first 5 initially, so the first time the autocomplete is filled, it'll only have a few to bind
+                results = allTags.slice(0, 5);
+            }
+            else if (/^-$/.test(searchTag)) {
+                // allow getting all values if they type just a "-"
+                results = allTags;
+            }
+            else {
+                searchTag = searchTag.toLowerCase();
+                results = allTags.filter(function (val) {
+                    return val.toLowerCase().indexOf(searchTag) === 0;
+                });
+            }
+            return results;
+        };
+        BasePersonalInfoController.prototype.submitBasePersonalInfo = function () {
+            var result = this.holder;
+        };
         return BasePersonalInfoController;
     }());
     BasePersonalInfoController.$inject = ['dataToolsService'];
--- src/vegetable.admin/wwwroot/app/src/controllers/basePersonalInfoController.ts
+++ src/vegetable.admin/wwwroot/app/src/controllers/basePersonalInfoController.ts
@@ -14,6 +14,32 @@ module AdminApp {
             });
         }
 
+        queryTags(searchTag: string): string[]
+        {
+            var results: string[];
+            let allTags: string[] = this.availableTabs;
+
+            if (!searchTag) {
+                // only return the first 5 initially, so the first time the autocomplete is filled, it'll only have a few to bind
+                results = allTags.slice(0, 5);
+            }
+            else if (/^-$/.test(searchTag)) {
+                // allow getting all values if they type just a "-"
+                results = allTags;
+            } else {
+                searchTag = searchTag.toLowerCase();
+
+                results = allTags.filter((val) => {
+                    return val.toLowerCase().indexOf(searchTag) === 0;
+                });
+            }
+
+            return results;
+        }
+
+        submitBasePersonalInfo(): void {
+            var result: BasePersonalInfo = this.holder;
+        }
         
        
     }
